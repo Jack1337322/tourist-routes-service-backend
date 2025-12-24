acae03ff63d6ac95abed7039b6fc8d021447aed4
r1natG <40369936+Jack1337322@users.noreply.github.com>
2025-12-24 21:08:34 +0300

    Add test endpoint to accounts URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 urlpatterns = [
+    path('test/', views.test, name='test'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
